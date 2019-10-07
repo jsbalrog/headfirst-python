29b34c58318ca74c2152ab451edc99a66972e433
Ted Jenkins <tjenkins@saltstack.com>
2019-10-07 09:16:33 -0600

    add logging

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -15,6 +15,7 @@ def do_search() -> str:
     letters = request.form['letters']
     title = 'Here are your results'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                            the_phrase=phrase,
                            the_letters=letters,
@@ -28,4 +29,9 @@ def entry_page() -> 'html':
                            the_title='Welcome to search4letters!')
 
 
-app.run(debug=True)
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
+if __name__ == '__main__':
+    app.run(debug=True)
